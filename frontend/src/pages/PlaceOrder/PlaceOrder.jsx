8de2c048e6f102e76a009a77302ba10bdc38e8bf
sidharth-webdev <shidharthpradhan999@gmail.com>
2026-03-10 23:26:02 +0530

    Fix order

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import './PlaceOrder.css'
 import { StoreContext } from '../../context/StoreContext'
 import axios from "axios"
+import { useNavigate } from "react-router-dom";
 
 const PlaceOrder = () => {
 
@@ -127,4 +128,4 @@ const navigate = useNavigate();
   )
 }
 
-export default PlaceOrder;         
+export default PlaceOrder;           
