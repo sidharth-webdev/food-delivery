0e2ce3edd8a411776f4d3b5518bd5aa6cae9bf88
sidharth-webdev <shidharthpradhan999@gmail.com>
2026-03-12 02:54:41 +0530

    placeorder fix

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -26,44 +26,39 @@ const PlaceOrder = () => {
     setData(prev => ({ ...prev, [name]: value }))
   }
 
-
-const placeOrder = async (event) => {
+  const placeOrder = async (event) => {
     event.preventDefault();
     let orderItems = [];
-    food_list.map((item)=>{
-       if(cartItems[item._id]>0){
-          let itemInfo = item;
-          itemInfo["quantity"] = cartItems[item._id];
-          orderItems.push(itemInfo)
-       }
-    })
+    food_list.map((item) => {
+      if (cartItems[item._id] > 0) {
+        let itemInfo = { ...item }; // ✅ FIX 2: spread to avoid mutation
+        itemInfo["quantity"] = cartItems[item._id];
+        orderItems.push(itemInfo);
+      }
+    });
     let orderData = {
-      address:data,
-      items:orderItems,
-      amount:getTotalCartAmount()+2,
-    }
-    let response = await axios.post(url+"/api/order/place",orderData,{headers:{token}});
-    if(response.data.success){
-        const {session_url} = response.data;
-        window.location.replace(session_url);
-    }
-    else{
+      address: data,
+      items: orderItems,
+      amount: getTotalCartAmount() + 2,
+    };
+    let response = await axios.post(url + "/api/order/place", orderData, { headers: { token } });
+    if (response.data.success) {
+      const { session_url } = response.data;
+      window.location.replace(session_url);
+    } else {
       alert("Error");
     }
-}
-  
-const navigate = useNavigate();
+  }
 
+  const navigate = useNavigate();
 
-  useEffect(()=>{
-   if(!token){
-      navigate('/cart')
-   }
-   else if(getTotalCartAmount===0)
-   {
-     navigate('/cart')
-   }
-  },[token])
+  useEffect(() => {
+    if (!token) {
+      navigate('/cart');
+    } else if (getTotalCartAmount() === 0) { // ✅ FIX 1: added () to call the function
+      navigate('/cart');
+    }
+  }, [token])
 
   return (
     <form onSubmit={placeOrder} className='place-order'>
@@ -73,8 +68,8 @@ const navigate = useNavigate();
         <p className='title'>Delivery Information</p>   
 
         <div className='multi-fields'>
-          <input  name='firstName' onChange={onChangeHandler} value={data.firstName} type='text' placeholder='First name' required/>
-          <input  name='lastName' onChange={onChangeHandler} value={data.lastName} type='text' placeholder='Last name' required/>
+          <input name='firstName' onChange={onChangeHandler} value={data.firstName} type='text' placeholder='First name' required/>
+          <input name='lastName' onChange={onChangeHandler} value={data.lastName} type='text' placeholder='Last name' required/>
         </div>
         <div className='multi-fields'>  
           <input name='email' onChange={onChangeHandler} value={data.email} type='email' placeholder='Email address' required />
@@ -82,13 +77,13 @@ const navigate = useNavigate();
         </div>
 
         <div className='multi-fields'>
-          <input  name='city' onChange={onChangeHandler} value={data.city} type='text' placeholder='City' required />
-          <input  name='state' onChange={onChangeHandler} value={data.state} type='text'  placeholder='State' required />
+          <input name='city' onChange={onChangeHandler} value={data.city} type='text' placeholder='City' required />
+          <input name='state' onChange={onChangeHandler} value={data.state} type='text' placeholder='State' required />
         </div>
 
         <div className='multi-fields'>
-          <input  name='zipcode' onChange={onChangeHandler} value={data.zipcode} type='text' placeholder='Zip code' required />
-          <input  name='country' onChange={onChangeHandler} value={data.country} type='text' placeholder='Country' required /> 
+          <input name='zipcode' onChange={onChangeHandler} value={data.zipcode} type='text' placeholder='Zip code' required />
+          <input name='country' onChange={onChangeHandler} value={data.country} type='text' placeholder='Country' required /> 
         </div>
         <input name='phone' onChange={onChangeHandler} value={data.phone} type='text' placeholder='Phone' required />
       </div>
@@ -128,4 +123,4 @@ const navigate = useNavigate();
   )
 }
 
-export default PlaceOrder;           
+export default PlaceOrder; 
