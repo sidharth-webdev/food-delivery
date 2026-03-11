f62d69827ce06a7587ed50e6f243c951daf7f535
sidharth-webdev <shidharthpradhan999@gmail.com>
2026-03-12 03:10:21 +0530

    fix jwt middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,18 +1,16 @@
 import jwt from "jsonwebtoken";
-
-const authMiddleware = async (req,res,next) => {
-     const {token} = req.headers;
-     if(!token){
-        return res.json({success:false,message:"Not Authorized Login Again"})
-     }
-     try {
-         const token_decode = jwt.verify(token,process.env.jwt_SECRET);
-         req.body.userId = token_decode.id;
-         next();
-     } catch (error){
-        console.log(error)
-        res.json({success:false,message:"Error"})
-     }
-}
-
+const authMiddleware = async (req, res, next) => {
+  const { token } = req.headers;
+  if (!token) {
+    return res.json({ success: false, message: "Not Authorized Login Again" });
+  }
+  try {
+    const token_decode = jwt.verify(token, process.env.JWT_SECRET); // ✅ fixed
+    req.body.userId = token_decode.id;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 export default authMiddleware; 
