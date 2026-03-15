a458e45b46164477e4e636404fe7578a14555b96
sidharth-webdev <shidharthpradhan999@gmail.com>
2026-03-16 03:03:11 +0530

    resolved merge conflict

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -11,4 +11,4 @@ const orderSchema = new mongoose.Schema({
 })
 
 const orderModel = mongoose.models.order || mongoose.model("order",orderSchema);
-export default orderModel; 
+export default orderModel;  
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -8,4 +8,4 @@ const userSchema = new mongoose.Schema({
 },{minimize:false})
 
 const userModel = mongoose.models.user || mongoose.model("user",userSchema);
-export default userModel;
+export default userModel;   
