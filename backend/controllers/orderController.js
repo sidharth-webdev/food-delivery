04b7f902be673cdeda62e6a983951cb480ffc83a
sidharth-webdev <shidharthpradhan999@gmail.com>
2026-03-19 23:42:49 +0530

    fix the url

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -4,7 +4,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // placing user order for frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "https://food-delivery-catvkyx8w-shidharthpradhan999-8337s-projects.vercel.app";
+  const frontend_url = "https://food-delivery-brown-seven.vercel.app";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
